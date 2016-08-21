72c3346e96243d7684fd8e61dc4ce3b25797a126
Aung Ko Ko <aungkokomyintoo@gmail.com>
2016-08-21 12:09:44 +0700

    Complete

---
+++ app/Contact.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+	protected $fillable=[
+	'name','email','address','company','phone','group_id','photo'
+	];
+
+    public function group(){
+    	return $this->belongsTo('App\Group');
+    }
+}
